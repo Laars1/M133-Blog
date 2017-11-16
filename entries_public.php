858a581b5ea47a0a699723c4f43295016b26ab2c
Laars1 <lars.bischhausen@gmail.com>
2017-11-16 15:28:43 +0100

    show blof

--- entries_public.php
+++ entries_public.php
@@ -3,8 +3,18 @@
   // Hier Code... (Schlaufe über alle Einträge dieses Blogs)
 
   // Nachfolgend das Beispiel einer Ausgabe in HTML, dieser Teil muss mit einer Schlaufe über alle Blog-Beiträge und der Ausgabe mit PHP ersetzt werden
+
+$uid = $_GET['bid'];
+$entries = getEntries($uid);
+
+foreach ($entries as $entry){
+	
+	$timestamp= $entry['datetime'];
+	$time = gmdate("d-m-Y H:i:s", $timestamp);
+	$title = $entry['title'];
+	$content = $entry['content'];
+	
+	echo $title." ".$time."<br>".$content;
+}
+
 ?>
-  <div>
-  <h4>Hipster Ipsum, 01.11.2016 16:42:12</h4>
-  Neutra truffaut blog, 90's microdosing gochujang fingerstache helvetica etsy. Shoreditch fashion axe tote bag wayfarers normcore, freegan hot chicken sriracha 8-bit brunch actually live-edge quinoa. Trust fund sustainable forage tilde, etsy gentrify 8-bit poutine blog swag lomo pug. Truffaut ugh pinterest, umami tofu hoodie cronut. Crucifix skateboard single-origin coffee, vape slow-carb pork belly direct trade everyday carry photo booth schlitz venmo franzen. Air plant viral stumptown pabst disrupt. Readymade mumblecore tumeric kitsch hashtag, godard trust fund.
-  </div>
