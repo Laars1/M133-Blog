5ed434d3212fbc55ca1da3b6cc9becb3c1c3c5cd
Laars1 <lars.bischhausen@gmail.com>
2017-11-23 14:58:21 +0100

    cool

--- blogs.php
+++ blogs.php
@@ -7,13 +7,21 @@
 
   // Nachfolgend das Beispiel einer Ausgabe in HTML, dieser Teil muss mit einer Schlaufe Ã¼ber alle Blogs und der Ausgabe mit PHP ersetzt werden
   
+
+	$colors = ['#CCE5FF', "#CCCCFF", "#FFCCFF", "#FFCCCC", "#FFE5CC", "#E5FFCC", "#CCFFFF", "#FFCCE5"];
+	
+	
+	
+	$number = mt_rand (0 ,7);
+	
+  
 	$blogs = getUserNames();
 	$blogId = $_GET['bid'];
 
 foreach ($blogs as $blog){
 
 	if($blog['uid'] == $blogId){
-		echo "<div class='autor' style='background-color:#FF00FF'><a href='index.php?function=blogs&bid=".$blog['uid']."' title='Blog auswählen'><h4>".$blog['name']."</h4></a></div>";
+		echo "<div class='autor' style='background-color:".$colors[$number]."'><a href='index.php?function=blogs&bid=".$blog['uid']."' title='Blog auswählen'><h4>".$blog['name']."</h4></a></div>";
 	}
 	else{
 		echo "<div class='autor'><a href='index.php?function=blogs&bid=".$blog['uid']."' title='Blog auswählen'><h4>".$blog['name']."</h4></a></div>";
