ade279252dae81ecb7e9679adfb20436f9758120
fabixn <13maurelio13@gmail.com>
2017-11-30 15:01:04 +0100

    weri kuhl tscheinsches

--- blogs.php
+++ blogs.php
@@ -8,8 +8,8 @@
   // Nachfolgend das Beispiel einer Ausgabe in HTML, dieser Teil muss mit einer Schlaufe Ã¼ber alle Blogs und der Ausgabe mit PHP ersetzt werden
   
 
-	$colors = ['#CCE5FF', "#CCCCFF", "#FFCCFF", "#FFCCCC", "#FFE5CC", "#E5FFCC", "#CCFFFF", "#FFCCE5"];
-	$number = mt_rand (0 ,7);
+	$colors = "#CCFFFF";
+
 	
   
 	$blogs = getUserNames();
@@ -24,7 +24,7 @@
 		foreach ($blogs as $blog){
 			
 			if($blog['uid'] == $blogId){
-				echo "<div class='autor' style='background-color:".$colors[$number]."'><a href='index.php?function=blogs&bid=".$blog['uid']."' title='Blog auswählen'><h4>".$blog['name']."</h4></a></div>";
+				echo "<div class='autor' style='background-color:".$colors."'><a href='index.php?function=blogs&bid=".$blog['uid']."' title='Blog auswählen'><h4>".$blog['name']."</h4></a></div>";
 			}
 			else{
 				echo "<div class='autor'><a href='index.php?function=blogs&bid=".$blog['uid']."' title='Blog auswählen'><h4>".$blog['name']."</h4></a></div>";
