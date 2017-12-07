2d13b5f8b4c4bb337d221e996162e1efe21ec4b4
Laars1 <lars.bischhausen@gmail.com>
2017-12-07 15:16:01 +0100

    asdef

--- blogCreate.php
+++ blogCreate.php
@@ -1,6 +1,7 @@
 <?php
 $eidSet = false;
 
+
 #Get UserID
 if (isset ( $_GET ['bid'] )) {
 	$uid = $_GET ['bid'];
@@ -13,8 +14,10 @@ if (isset ( $_GET ['eid'] )) {
 	$eid = $_GET ['eid'];
 	$eidSet = true;
 	$url = $_SERVER ['PHP_SELF'] . "?function=blogcreate&uid=" . $uid . "&eid=" . $eid;
+	$urlDelete = $url = $_SERVER ['PHP_SELF'] . "?function=blogcreate&uid=" . $uid . "&eid=" . $eid."&action=delete";
 } else {
 	$url = $_SERVER ['PHP_SELF'] . "?function=blogcreate&uid=" . $uid;
+	
 }
 
 #Check if eid is set
@@ -27,31 +30,76 @@ if ($eidSet == true) {
 	$content = "";
 }
 
-if (ISSET ( $_POST ['titel'] ) == True && isset ( $_POST ['inhalt'] ) == True) {
-	$titelnew = $_POST ['titel'];
-	$inhalt = $_POST ['inhalt'];
-	
-	if (strlen ( $titelnew ) > 3 && strlen ( $inhalt ) > 10) {
-		if ($eidSet == true) {
-			// Datei aktualisieren
-			echo "<div class='feedbackOK'>";
-			echo "<p class='ok'>Ihr Beitrag wurde erflogreich aktualisiert</p>";
-			echo "<i class='fa fa-check-circle-o fa-2x' aria-hidden='true'></i>";
-			echo "</div>";
-		} else {
-			// Neue Datei erstellen
-			echo "<div class='feedbackOK'>";
-			echo "<p class='ok'>Ihr Beitrag wurde erflogreich erstellt</p>";
-			echo "<i class='fa fa-check-circle-o fa-2x' aria-hidden='true'></i>";
-			echo "</div>";
-		}
-	} else {
+
+//Check if user want to delete blog or create/update blog
+if (isset($_GET['action'])){
+	//Delete blog
+	if (deleteEntry($eid) == true){
+		echo "<div class='feedbackOK'>";
+		echo "<p class='ok'>Ihr Beitrag wurde erflogreich gelöscht</p>";
+		echo "<i class='fa fa-check-circle-o fa-2x' aria-hidden='true'></i>";
+		echo "</div>";
+	}
+	else{
 		echo "<div class='feedbackNOK'>";
-		echo "<p id='nok'>Einer oder beide Texte sind zu kurz!</p>";
+		echo "<p id='nok'>Löschen Fehlgeschlagen</p>";
 		echo "<i class='fa fa-times fa-2x' aria-hidden='true'></i>";
 		echo "</div>";
 	}
 }
+else{
+	if (ISSET ( $_POST ['titel'] ) == True && isset ( $_POST ['inhalt'] ) == True) {
+		$titelnew = $_POST ['titel'];
+		$inhalt = $_POST ['inhalt'];
+		
+		if (strlen ( $titelnew ) > 3 && strlen ( $inhalt ) > 10) {
+			if ($eidSet == true) {
+				// Datei aktualisieren
+				
+				
+				if (updateEntry($eid, $titelnew, $inhalt)== true){
+					echo "<div class='feedbackOK'>";
+					echo "<p class='ok'>Ihr Beitrag wurde erflogreich aktualisiert</p>";
+					echo "<i class='fa fa-check-circle-o fa-2x' aria-hidden='true'></i>";
+					echo "</div>";
+				}
+				else{
+					echo "<div class='feedbackNOK'>";
+					echo "<p id='nok'>Hochladen Fehlgeschlagen</p>";
+					echo "<i class='fa fa-times fa-2x' aria-hidden='true'></i>";
+					echo "</div>";
+				}
+				
+			} else {
+				// Neue Datei erstellen
+				
+				
+				if (addEntry($uid, $titelnew, $inhalt)== true){
+					echo "<div class='feedbackOK'>";
+					echo "<p class='ok'>Ihr Beitrag wurde erflogreich erstellt</p>";
+					echo "<i class='fa fa-check-circle-o fa-2x' aria-hidden='true'></i>";
+					echo "</div>";
+				}
+				else{
+					echo "<div class='feedbackNOK'>";
+					echo "<p id='nok'>Fehler beim Hochladen</p>";
+					echo "<i class='fa fa-times fa-2x' aria-hidden='true'></i>";
+					echo "</div>";
+				}
+				
+				
+			}
+		} else {
+			echo "<div class='feedbackNOK'>";
+			echo "<p id='nok'>Einer oder beide Texte sind zu kurz!</p>";
+			echo "<i class='fa fa-times fa-2x' aria-hidden='true'></i>";
+			echo "</div>";
+		}
+	}
+}
+
+
+
 ?>
 
 <div class="blogcrete">
@@ -66,6 +114,12 @@ if (ISSET ( $_POST ['titel'] ) == True && isset ( $_POST ['inhalt'] ) == True) {
 		</div>
 
 		<button type="submit" id="submitButton">senden</button>
+	</form>
+	<form method="post" action="<?php echo $urlDelete; ?>">
+	<button type="submit" id="submitButton">löschen</button>
+	</form>
+		<form method="post" action="<?php 	echo "index.php?function=entries_public"; ?>">
+	<button type="submit" id="submitButton">Abbrechen</button>
 	</form>
 
 </div>
